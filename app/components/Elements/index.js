46ea44e0825a0711e8e520f5b332ec81d269620d
lukabajic <lukabajic.dev@gmail.com>
2021-06-20 22:01:55 +0200

    Remove stuff from typography elements

--- app/components/Elements/index.js
+++ app/components/Elements/index.js
@@ -9,18 +9,12 @@ import styled from 'styled-components';
 export const H1 = styled.h1`
   font-size: 4rem;
   font-weight: 300;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
   letter-spacing: -0.8px;
 `;
 
 export const H2 = styled.h2`
   font-size: 3.6rem;
   font-weight: 300;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
   letter-spacing: -0.72px;
   text-align: center;
 `;
@@ -28,18 +22,12 @@ export const H2 = styled.h2`
 export const H3 = styled.h3`
   font-size: 3.4rem;
   font-weight: 300;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
   letter-spacing: -0.68px;
 `;
 
 export const H4 = styled.h4`
   font-size: 3rem;
   font-weight: 300;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
   letter-spacing: -0.72px;
   text-align: center;
 `;
@@ -47,8 +35,5 @@ export const H4 = styled.h4`
 export const Footnote = styled.p`
   font-size: 1.5rem;
   font-weight: 300;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
   letter-spacing: -0.3px;
 `;
