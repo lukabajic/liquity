359e36b877f7d0146300c9030f5941cd368d664f
lukabajic <lukabajic.dev@gmail.com>
2021-06-20 21:21:40 +0200

    Add nav link custom component

---
+++ app/components/Link/index.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * Link
+ *
+ */
+
+import React, { memo } from 'react';
+import { NavLink } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+
+const Link = ({ children, href, activeClassName, to, ...rest }) => {
+  if (href)
+    return (
+      <a href={href} {...rest} target="_blank" rel="noreferrer">
+        {children}
+      </a>
+    );
+
+  return (
+    <NavLink exact activeClassName={activeClassName} to={to} {...rest}>
+      {children}
+    </NavLink>
+  );
+};
+
+Link.propTypes = {
+  to: PropTypes.string,
+  href: PropTypes.string,
+  activeClassName: PropTypes.string,
+};
+
+export default memo(Link);
